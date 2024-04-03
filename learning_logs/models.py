fe649c06602cadcd2dffc407bbfdbee2ad973357
MoldashAmanov <amanov1990@yandex.ru>
2024-04-03 22:47:19 +0500

    Создание главной страницы

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,15 @@ def __str__(self):
     """Возвращает строковое представление модели"""
     return self.text
 
+class Entry(models.Model):
+    """Информация, изученная пользователями по теме"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return f"{self.text[:50]}..."
