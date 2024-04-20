1f46fb4048fb964420eff50629837cfd2f6a3002
MoldashAmanov <amanov1990@yandex.ru>
2024-04-20 18:36:24 +0500

    Добавление редактирования страниц

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 def index(request):
@@ -12,12 +12,14 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
 def topic(request, topic_id):
     """Выводит одну тему и ее записи"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
 
 def new_topic(request):
     """Определяет новую тему"""
@@ -33,6 +35,7 @@ def new_topic(request):
     # Вывести пустую или недействительную форму
     context = {'form': form}
     return render(request, 'learning_logs/new_topics.html', context)
+
 
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме"""
@@ -52,4 +55,23 @@ def new_entry(request, topic_id):
     # Вывести пустую или недействительную форму
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Редактирует существующую запись"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Исходный запрос; форма заполняется данными текущей записи
+        form = EntryForm(instance=entry)
+    else:
+        # Отправка данных POST; обработать данные
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
 
