912b8e279c132482aa729143f7d7433bceee67ee
MoldashAmanov <amanov1990@yandex.ru>
2024-04-25 17:08:04 +0500

    Добавление защиты тем пользователей

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import Http404
 
 def index(request):
     """Домашняя страница приложения learning log"""
@@ -10,7 +11,7 @@ def index(request):
 @login_required
 def topics(request):
     """Выводит список тем"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -18,6 +19,9 @@ def topics(request):
 def topic(request, topic_id):
     """Выводит одну тему и ее записи"""
     topic = Topic.objects.get(id=topic_id)
+    # Проверка того, что тема принадлежит пользователю
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -32,7 +36,9 @@ def new_topic(request):
         # Отправлены данные POST; обработать данные
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # Вывести пустую или недействительную форму
     context = {'form': form}
@@ -63,7 +69,9 @@ def edit_entry(request, entry_id):
     """Редактирует существующую запись"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-
+    if topic.owner != request.user:
+        raise Http404
+    
     if request.method != 'POST':
         # Исходный запрос; форма заполняется данными текущей записи
         form = EntryForm(instance=entry)
